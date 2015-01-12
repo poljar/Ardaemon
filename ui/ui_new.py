a9dec36bb3e9c0e0effbe46efd3ea7fbb2470ff4
poljar (Damir Jelić) <poljarinho@gmail.com>
2015-01-12 23:14:28 +0100

    ui: Add rudimentary command history.

--- ui/ui_new.py
+++ ui/ui_new.py
@@ -13,6 +13,8 @@ class CommandLine(urwid.Pile):
 
     Attributes:
         commands: A dictionary containing command names and fucntions.
+        history: Hisotry of last 20 entered commands.
+        hist_pos: Currently selected position in the history.
         cmd: The input line widget.
         status: The status line widget.
         attr_map: Attribution map for the colored status line.
@@ -21,6 +23,8 @@ class CommandLine(urwid.Pile):
     def __init__(self, commands):
         """Inits CommandLine widget with a dict of commands."""
         self.commands = commands
+        self.history = []
+        self.hist_pos = None
 
         self.cmd = urwid.Edit(caption='λ: ')
         self.status = urwid.Text('')
@@ -30,11 +34,38 @@ class CommandLine(urwid.Pile):
 
     def keypress(self, size, key):
         """Handle keypresses and add emacs like keybindings."""
+
+        def cursor_to_end():
+            self.cmd.set_edit_pos(len(self.cmd.edit_text))
+
         if key == 'enter':
             cmd = self.cmd.edit_text.split()
 
-            self.handle_command(size, cmd[0], cmd[1:])
-            self.cmd.set_edit_text('')
+            if cmd:
+                self.history.append(self.cmd.edit_text)
+                self.hist_pos = None
+
+                self.handle_command(size, cmd[0], cmd[1:])
+                self.cmd.set_edit_text('')
+
+        elif key == 'ctrl p':
+            if self.history:
+                if self.hist_pos is None:
+                    self.hist_pos = len(self.history) - 1
+                    self.cmd.set_edit_text(self.history[self.hist_pos])
+                    cursor_to_end()
+
+                elif self.hist_pos > 0:
+                    self.hist_pos = self.hist_pos - 1
+                    self.cmd.set_edit_text(self.history[self.hist_pos])
+                    cursor_to_end()
+
+        elif key == 'ctrl n':
+            if self.history:
+                if self.hist_pos < len(self.history) - 1:
+                    self.hist_pos = self.hist_pos + 1
+                    self.cmd.set_edit_text(self.history[self.hist_pos])
+                    cursor_to_end()
 
         elif key == 'ctrl u':
             self.cmd.set_edit_text('')
@@ -51,7 +82,7 @@ class CommandLine(urwid.Pile):
             self.cmd.set_edit_pos(0)
 
         elif key == 'ctrl e':
-            self.cmd.set_edit_pos(len(self.cmd.edit_text))
+            cursor_to_end()
 
         elif key == 'ctrl j':
             return self.keypress(size, 'enter')
