056e086239f24e686fd9b45666a0b3b4e5671736
poljar (Damir Jelić) <poljar@termina.org.uk>
2015-03-19 11:22:35 +0100

    daemon: Add a controller broker and a prototype control loop.
    The broker reads from the channel containing the reference values set by
    different clients and forwards it to the control loop.

--- daemon/Main.hs
+++ daemon/Main.hs
@@ -8,7 +8,7 @@ import Control.Concurrent.MVar
 import Control.Concurrent.Chan
 import Control.Exception
 import Control.Monad.Reader
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 
 import Network
 import Network.JsonRpc.Server (call)
@@ -37,9 +37,12 @@ options = Options {
 main :: IO ()
 main = withSocketsDo $ do
     Options {..} <- cmdArgs options
+
     pv <- newMVar 20
     referenceChan <- newChan
     let com = ProcCom pv referenceChan
+
+    _ <- forkIO $ controllerBroker com
 
     sock <- listenOn $ PortNumber $ fromInteger port
     mainLoop sock com
@@ -50,6 +53,26 @@ mainLoop sock com = do
     (hdl, _, _) <- accept sock
     _ <- forkIO $ runConn hdl com
     mainLoop sock com
+
+
+controllerBroker :: ProcCom Int -> IO ()
+controllerBroker (ProcCom pvMVar refChan) = do
+    refMVar <- newMVar 0
+    _ <- forkIO $ controlLoop refMVar pvMVar
+
+    forever $ do
+        ref <- readChan refChan
+        swapMVar refMVar ref
+
+
+controlLoop :: MVar Int -> MVar Int -> IO ()
+controlLoop refMVar pvMVar = forever $ do
+    ref <- readMVar refMVar
+    -- measure the PV here
+    _ <- swapMVar pvMVar 15
+    -- implement a controler here
+    print ref
+    threadDelay 1000000
 
 
 handleMsg :: ProcCom Int -> C.ByteString -> IO C.ByteString
