3d0497ac642edbb465a2c6c6e617b0fc66e7bc5e
poljar (Damir Jelić) <poljar@termina.org.uk>
2015-04-27 16:46:51 +0200

    daemon: Refactor the arduino stuff.

--- daemon/Main.hs
+++ daemon/Main.hs
@@ -20,13 +20,9 @@ import System.IO (Handle, hGetLine, hIsEOF, hClose)
 
 import qualified Data.ByteString.Lazy.Char8 as C
 
-import System.Hardware.Arduino
-import System.Hardware.Serialport
-import qualified Data.ByteString as B
-import Data.Word (Word8)
-
 
 import Commands
+import Arduino
 
 
 data Options = Options {
@@ -90,72 +86,9 @@ controllerBroker arduinoPort simulate (ProcCom pvMVar refChan) = do
         simulatorLoop refMVar pvMVar
     else do
         controlLoop arduinoPort refMVar pvMVar
-        port <- openSerial arduinoPort defaultSerialSettings { commSpeed = CS57600 }
-        _ <- send port $ B.pack [0xFF :: Word8]
-        closeSerial port
+        shutDownArduino arduinoPort
 
     putStrLn "Shutting daemon down."
-
-
-controlLoop :: FilePath -> MVar PVType -> MVar PVType -> IO ()
-controlLoop arduinoPath refMVar pvMVar = withArduino False arduinoPath $ do
-        liftIO $ putStrLn "Daemon startup finished"
-        let sensor      = analog 0
-        let pwm_pin     = digital 3
-        let chip_enable = digital 2
-
-        setPinMode sensor ANALOG
-        setPinMode pwm_pin PWM
-        setPinMode chip_enable OUTPUT
-        digitalWrite chip_enable True
-
-        integralMVar <- liftIO $ newMVar 0
-
-        forever $ do
-            integral  <- liftIO $ takeMVar integralMVar
-            reference <- liftIO $ readMVar refMVar
-
-            sensorValue <- analogRead sensor
-            let fillHeight = sensorValueFunc sensorValue
-            _ <- liftIO $ swapMVar pvMVar fillHeight
-
-            let error = reference - fillHeight
-
-            let proportional_term = kp * error
-
-            let newIntegral = if proportional_term < maxOut &&
-                                 proportional_term > minOut then
-                                  (integral + error / fromIntegral sampleTime) * ki
-                              else
-                                  integral
-
-            let output = clampAndScaleOutput $ kp * error + newIntegral
-
-            liftIO $ putMVar integralMVar newIntegral
-
-            liftIO $ putStrLn ("Ref: " ++ show reference ++ " sensorValue: "
-                               ++ show sensorValue ++ " height: " ++ show fillHeight)
-
-            liftIO $ putStrLn ("Error: " ++ show error ++ " integral: " ++ show newIntegral)
-
-            analogWrite pwm_pin output
-            delay sampleTime
-        where
-                kp = 4
-                ki = 1
-                minOut = 3
-                maxOut = 12
-                sampleTime = 100
-                clampAndScaleOutput out
-                    | out <  1.5      = 0
-                    | out <  minOut   = 64
-                    | out >= maxOut   = 255
-                    | otherwise       = round $ (out / 12) * 255
-
-
-sensorValueFunc :: Int -> Double
-sensorValueFunc value = fromIntegral value * sensor_constant
-                            where sensor_constant = 0.0296
 
 
 simulatorLoop :: MVar PVType -> MVar PVType -> IO ()
