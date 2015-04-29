e655a92f82103b01e2b388073cca4a21cf36fc81
poljar (Damir Jelić) <poljar@termina.org.uk>
2015-04-29 20:07:45 +0200

    client: Fix the incorrect author line in setup.py

--- client/setup.py
+++ client/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(name = 'arclient',
       version = '0.1',
-      author = 'Damir Jelić, Denis Kasak',
-      author_email = 'poljar[at]termina.org.uk, dkasak[at]termina.org.uk',
+      author = 'Damir Jelić',
+      author_email = 'poljar[at]termina.org.uk',
       description = ('A simple plumber'),
       install_requires = ['urwid', 'drawille'],
       packages = ['arclient'],
